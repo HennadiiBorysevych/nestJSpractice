ba48967d5519327d91cd4ac90c5ca5eb0f0b3a00
Hennadii Borysevych <genuch3@gmail.com>
2024-05-02 17:35:03 +0200

    setup for messages

---
+++ src/messages/messages.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, Post } from '@nestjs/common';
+
+@Controller('messages')
+export class MessagesController {
+  @Get()
+  listMessages() {
+    return 'list of messages';
+  }
+  @Post()
+  createMessage() {
+    return 'new message';
+  }
+  @Get('/:id')
+  getMessage() {
+    return 'message';
+  }
+}
